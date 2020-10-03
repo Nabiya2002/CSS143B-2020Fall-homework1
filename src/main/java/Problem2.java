bf3e82cf186385f58227f52a2d5b3a5d6fdea58f
Nabiya Alam <nabiya@uw.edu>
2020-10-03 13:25:09 -0700

    First version of homework

--- src/main/java/Problem2.java
+++ src/main/java/Problem2.java
@@ -2,6 +2,15 @@ public class Problem2 {
 
     // Do not change signature (function name, parameters)
     public static void bubbleSort(int[] data) {
-        // Add your solution
+
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = i+1; j < data.length; j++) {
+                if (data[i] > data[j]) {
+                    int temp = data[i];
+                    data[i] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
     }
 }
