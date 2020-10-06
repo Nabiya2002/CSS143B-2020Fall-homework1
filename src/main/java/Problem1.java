300ca549565c02f384aa36c2adc87c87eefd513a
Nabiya Alam <nabiya@uw.edu>
2020-10-05 23:37:58 -0700

    submitting for review

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -1,8 +1,32 @@
+/*
+    Author: Nabiya Alam
+    Version: 1.0.0
+ */
 public class Problem1 {
 
     // Do not change signature (function name, parameters)
     public static int binarySearch(int[] data, int target) {
-        // Add your solution
+
+        int left = 0;
+        int right = data.length - 1;
+
+        while (left <= right) {
+
+            // find the middle position
+            int mid = (left + right) / 2;
+
+            // compare mid value with target
+            if (data[mid] == target) {
+                return mid;
+            }
+            // if target is greater than mid move left pointer else move right pointer
+            if (target > data[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // if target not found in array return -1
         return -1; // Place holder
     }
 }
--- src/test/java/Problem1Test.java
+++ src/test/java/Problem1Test.java
@@ -1,5 +1,9 @@
-import org.junit.Test;
+/*
+    Author: Nabiya Alam
+    Version: 1.0.0
+ */
 
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class Problem1Test {
@@ -8,25 +12,25 @@ public class Problem1Test {
     // DO NOT CHANGE
     @Test
     public void testBinarySearch() {
-        int inputs[][] = {
+        int[][] inputs = {
                 {},
                 {3},
                 {3},
-                {1,3},
-                {1,3},
-                {4,6,8},
-                {4,6,8},
-                {4,6,8},
-                {3,5,7,9,20},
-                {3,5,7,9,20},
+                {1, 3},
+                {1, 3},
+                {4, 6, 8},
+                {4, 6, 8},
+                {4, 6, 8},
+                {3, 5, 7, 9, 20},
+                {3, 5, 7, 9, 20},
         };
-        int targets[] = {9, 1, 3, 1, 3, 4, 6, 8, 3, 9};
-        int answer[] = {-1, -1, 0, 0, 1, 0, 1, 2, 0, 3};
+        int[] targets = {9, 1, 3, 1, 3, 4, 6, 8, 3, 9};
+        int[] answer = {-1, -1, 0, 0, 1, 0, 1, 2, 0, 3};
 
         assertEquals(inputs.length, targets.length);
         assertEquals(inputs.length, answer.length);
 
-        for (int i=0; i<inputs.length; i++) {
+        for (int i = 0; i < inputs.length; i++) {
             int actual = Problem1.binarySearch(inputs[i], targets[i]);
             int expected = answer[i];
             assertEquals(expected, actual);
